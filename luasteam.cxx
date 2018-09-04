9d47f5d3a1e05ba5343ed8892dd18f3b5563e0d0
rilifon <pudimdefrutas@gmail.com>
2018-09-04 16:33:58 -0300

    Added some Achievements functions + calling SteamInit
    Signed off by: @kazuo256

--- luasteam.cxx
+++ luasteam.cxx
@@ -8,6 +8,8 @@ extern "C" {
 }
 
 extern "C" {
+//GENERAL FUNCTIONS
+
 int luasteam_init(lua_State *L){
     lua_settop(L,0); //Reset lua stack
     lua_pushboolean(L,SteamAPI_Init());
@@ -18,11 +20,13 @@ int luasteam_shutdown(lua_State *L){
     return 0;
 }
 
+//ACHIEVEMENTS FUNCTIONS
+
 //bool GetUserAchievement(const char *pchName, bool *pbAchieved );
 int luasteam_getAchievement(lua_State *L){
     lua_settop(L,1);
     if (!lua_isstring(L,1)){
-        lua_pushstring(L, "incorrect type for argument2 (string expected)");
+        lua_pushstring(L, "incorrect type for argument1 (string expected)");
         return lua_error(L);
     }
     std::string ach_name = lua_tostring(L,1);
@@ -33,18 +37,52 @@ int luasteam_getAchievement(lua_State *L){
     lua_pushboolean(L,achieved);
     return 2;
 }
+
+//bool SetAchievement( const char *pchName );
+int luasteam_setAchievement(lua_State *L){
+    lua_settop(L,1);
+    if (!lua_isstring(L,1)){
+        lua_pushstring(L, "incorrect type for argument1 (string expected)");
+        return lua_error(L);
+    }
+    std::string ach_name = lua_tostring(L,1);
+    lua_settop(L,0);
+    bool success = SteamUserStats()->SetAchievement(ach_name.c_str());
+    lua_pushboolean(L,success);
+    return 1;
 }
 
+//bool ResetAllStats( bool bAchievementsToo );
+int luasteam_resetAllStats(lua_State *L){
+    lua_settop(L,1);
+    bool achievements_too = lua_toboolean(L,1);
+    lua_settop(L,0);
+    bool success = SteamUserStats()->ResetAllStats(achievements_too);
+    lua_pushboolean(L,success);
+    return 1;
+}
+}
+
+
 //Table with all our functions
-luaL_Reg luasteam_module[3] = {
+luaL_Reg luasteam_module[5] = {
     {"init",luasteam_init},
     {"shutdown",luasteam_shutdown},
-    {"getAchievement", luasteam_getAchievement}
+    {"getAchievement", luasteam_getAchievement},
+    {"setAchievement", luasteam_setAchievement},
+    {"resetAllStats", luasteam_resetAllStats}
 };
 
 extern "C" int luaopen_steam(lua_State *L){
     lua_createtable(L,0,sizeof(luasteam_module));
     luaL_register(L,nullptr,luasteam_module);
+    if (SteamAPI_Init()) {
+        printf("Sucessfully connected to steam!\n");
+    }else{
+        printf("Couldn't connect to steam...\nDo you have Steam turned on?\nIf not running from steam, do you have a correct steam_appid.txt file?\n");
+        lua_settop(L,0);
+        lua_pushboolean(L,false);
+    }
     return 1;
 }
 
