c01cd3d7b545eda08aa20d85e1b583e9eed1ef1c
Yan Couto <yancouto@gmail.com>
2018-11-30 11:02:00 -0200

    putting existing functions inside userStats

--- luasteam.cxx
+++ luasteam.cxx
@@ -83,28 +83,50 @@ EXTERN int luasteam_requestCurrentStats(lua_State *L){
     return 1;
 }
 
+EXTERN int luasteam_activateGameOverlay(lua_State *L) {
+    const char *dialog = lua_tostring(L, 1);
+    printf("Activating overlay with %s\n", dialog);
+    SteamFriends()->ActivateGameOverlay(dialog);
+    return 0;
+}
 
 
-//Table with all our functions
-luaL_Reg luasteam_module[] = {
-    {"init",luasteam_init},
-    {"shutdown",luasteam_shutdown},
-    {"getAchievement", luasteam_getAchievement},
-    {"setAchievement", luasteam_setAchievement},
-    {"resetAllStats", luasteam_resetAllStats},
-    {"storeStats", luasteam_storeStats},
-    {"requestCurrentStats", luasteam_requestCurrentStats},
-    { nullptr, nullptr }
-};
+void add_func(lua_State *L, const char *name, lua_CFunction func) {
+    lua_pushstring(L, name);
+    lua_pushcfunction(L, func);
+    lua_rawset(L, -3);
+}
+
+void add_user_stats(lua_State *L) {
+    lua_createtable(L, 0, 5);
+    add_func(L, "getAchievement", luasteam_getAchievement);
+    add_func(L, "setAchievement", luasteam_setAchievement);
+    add_func(L, "resetAllStats", luasteam_resetAllStats);
+    add_func(L, "storeStats", luasteam_storeStats);
+    add_func(L, "requestCurrentStats", luasteam_requestCurrentStats);
+    lua_pushstring(L, "userStats");
+    lua_insert(L, -2); // swap string and table
+    lua_rawset(L, -3);
+}
+
+void add_friends(lua_State *L) {
+    lua_createtable(L, 0, 1);
+    add_func(L, "activateGameOverlay", luasteam_activateGameOverlay);
+    lua_pushstring(L, "friends");
+    lua_insert(L, -2); // swap string and table
+    lua_rawset(L, -3);
+}
 
 EXTERN int luaopen_luasteam(lua_State *L){
-    lua_createtable(L,0,sizeof(luasteam_module));
-    luaL_register(L,nullptr,luasteam_module);
+    lua_createtable(L, 0, 4);
+    add_func(L, "init", &luasteam_init);
+    add_func(L, "shutdown", &luasteam_shutdown);
+    add_user_stats(L);
+    add_friends(L);
     if (SteamAPI_Init()) {
         printf("Sucessfully connected to steam!\n");
     }else{
         printf("Couldn't connect to steam...\nDo you have Steam turned on?\nIf not running from steam, do you have a correct steam_appid.txt file?\n");
-        lua_settop(L,0);
         lua_pushboolean(L,false);
     }
     return 1;
