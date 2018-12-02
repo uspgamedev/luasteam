4c7a47fb78e04135851f66d1d112a0af328357a6
Yan Couto <yancouto@gmail.com>
2018-12-02 18:44:44 -0200

    Adding OnGameOverlayActivated callback

--- luasteam.cxx
+++ luasteam.cxx
@@ -13,6 +13,10 @@ extern "C" {
 #include <lauxlib.h>
 }
 
+// ========================
+// ======= SteamAPI =======
+// ========================
+
 //GENERAL FUNCTIONS
 EXTERN int luasteam_init(lua_State *L){
     lua_settop(L,0); //Reset lua stack
@@ -25,7 +29,14 @@ EXTERN int luasteam_shutdown(lua_State *L){
     return 0;
 }
 
-//ACHIEVEMENTS FUNCTIONS
+EXTERN int luasteam_runCallbacks(lua_State *L) {
+    SteamAPI_RunCallbacks();
+    return 0;
+}
+
+// ==============================
+// ======= SteamUserStats =======
+// ==============================
 
 //bool GetUserAchievement(const char *pchName, bool *pbAchieved );
 EXTERN int luasteam_getAchievement(lua_State *L){
@@ -83,13 +94,48 @@ EXTERN int luasteam_requestCurrentStats(lua_State *L){
     return 1;
 }
 
+// ============================
+// ======= SteamFriends =======
+// ============================
+
 EXTERN int luasteam_activateGameOverlay(lua_State *L) {
-    const char *dialog = lua_tostring(L, 1);
-    printf("Activating overlay with %s\n", dialog);
+    const char *dialog = luaL_checkstring(L, 1);
     SteamFriends()->ActivateGameOverlay(dialog);
     return 0;
 }
 
+EXTERN int luasteam_activateGameOverlayToWebPage(lua_State *L) {
+    const char *url = luaL_checkstring(L, 1);
+    SteamFriends()->ActivateGameOverlayToWebPage(url);
+    return 0;
+}
+
+class SteamFriendsListener {
+    public:
+    SteamFriendsListener(lua_State *L_, int ref) : L(L_), friends_ref(ref) {}
+    private:
+    lua_State *L;
+    int friends_ref;
+    STEAM_CALLBACK(SteamFriendsListener, OnGameOverlayActivated, GameOverlayActivated_t);
+};
+
+void SteamFriendsListener::OnGameOverlayActivated(GameOverlayActivated_t *data) {
+    if(!lua_checkstack(L, 3)) return;
+    lua_rawgeti(L, LUA_REGISTRYINDEX, friends_ref);
+    lua_getfield(L, -1, "onGameOverlayActivated");
+    if(lua_isnil(L, -1)) {
+        lua_pop(L, 2);
+    } else {
+        lua_pushboolean(L, data->m_bActive);
+        lua_call(L, 1, 0);
+        lua_pop(L, 1);
+    }
+}
+// ====================================
+// ======= End of API functions =======
+// ====================================
+
+namespace {
 
 void add_func(lua_State *L, const char *name, lua_CFunction func) {
     lua_pushstring(L, name);
@@ -97,37 +143,47 @@ void add_func(lua_State *L, const char *name, lua_CFunction func) {
     lua_rawset(L, -3);
 }
 
+void add_base(lua_State *L) {
+    add_func(L, "init", luasteam_init);
+    add_func(L, "shutdown", luasteam_shutdown);
+    add_func(L, "runCallbacks", luasteam_runCallbacks);
+}
+
 void add_user_stats(lua_State *L) {
+    lua_pushstring(L, "userStats"); // name of the table, used in the end of the function
     lua_createtable(L, 0, 5);
     add_func(L, "getAchievement", luasteam_getAchievement);
     add_func(L, "setAchievement", luasteam_setAchievement);
     add_func(L, "resetAllStats", luasteam_resetAllStats);
     add_func(L, "storeStats", luasteam_storeStats);
     add_func(L, "requestCurrentStats", luasteam_requestCurrentStats);
-    lua_pushstring(L, "userStats");
-    lua_insert(L, -2); // swap string and table
     lua_rawset(L, -3);
 }
 
-void add_friends(lua_State *L) {
-    lua_createtable(L, 0, 1);
+int add_friends(lua_State *L) {
+    lua_pushstring(L, "friends"); // name of the table, used in the end of the function
+    lua_createtable(L, 0, 2);
     add_func(L, "activateGameOverlay", luasteam_activateGameOverlay);
-    lua_pushstring(L, "friends");
-    lua_insert(L, -2); // swap string and table
+    add_func(L, "activateGameOverlayToWebPage", luasteam_activateGameOverlayToWebPage);
+    lua_pushvalue(L, -1); // copying table
+    int id = luaL_ref(L, LUA_REGISTRYINDEX); // used in the listener
     lua_rawset(L, -3);
+    return id;
 }
+
+} // namespace
 
 EXTERN int luaopen_luasteam(lua_State *L){
-    lua_createtable(L, 0, 4);
-    add_func(L, "init", &luasteam_init);
-    add_func(L, "shutdown", &luasteam_shutdown);
+    lua_createtable(L, 0, 5);
+    add_base(L);
     add_user_stats(L);
-    add_friends(L);
+    int friends_ref = add_friends(L);
     if (SteamAPI_Init()) {
         printf("Sucessfully connected to steam!\n");
-    }else{
+    } else{
         printf("Couldn't connect to steam...\nDo you have Steam turned on?\nIf not running from steam, do you have a correct steam_appid.txt file?\n");
-        lua_pushboolean(L,false);
+        lua_pushboolean(L, false);
     }
+    new SteamFriendsListener(L, friends_ref);
     return 1;
 }
