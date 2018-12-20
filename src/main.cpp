a8ee36b06fb9fc5dd116bfb9ec3adf2bb3fffb08
Yan Couto <yancouto@gmail.com>
2018-12-19 22:11:09 -0200

    Added user.getSteamID and user.getPlayerSteamLevel

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,15 @@
 #include "common.hpp"
 #include "core.hpp"
 #include "friends.hpp"
+#include "user.hpp"
 #include "user_stats.hpp"
 
 // Creates and returns a table with all functions
 EXTERN int luaopen_luasteam(lua_State *L) {
-    lua_createtable(L, 0, 5);
+    lua_createtable(L, 0, 6);
     luasteam::add_core(L);
-    luasteam::add_user_stats(L);
     luasteam::add_friends(L);
+    luasteam::add_user(L);
+    luasteam::add_user_stats(L);
     return 1;
 }
