1821af7d21b7753b773e17361cfb05e834fb68b4
Yan Couto <yancouto@gmail.com>
2018-12-26 12:31:19 -0300

    PlaytimeTracking

--- src/common.hpp
+++ src/common.hpp
@@ -21,6 +21,7 @@ extern lua_State *global_lua_state;
 // using userdata since Lua's number can't safely hold 64-bit integers
 void pushuint64(lua_State *L, uint64 v);
 uint64 checkuint64(lua_State *L, int nParam);
+uint64 assertuint64(lua_State *L, int index, const char *fmt, ...);
 
 // Adds a C function to the table on top of the stack, with given name
 void add_func(lua_State *L, const char *name, lua_CFunction func);
