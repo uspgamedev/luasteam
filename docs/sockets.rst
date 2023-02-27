5b81e753d4cd1ab705312df223ecee9674fc5cd9
Sebastian Tauch <sebastian.tauch@aborium.com>
2023-02-27 20:26:03 +0900

    Implement acceptConnection and connectionChanged callback

--- docs/sockets.rst
+++ docs/sockets.rst
@@ -6,6 +6,8 @@ ISteamNetworkingSockets
 List of Functions
 -----------------
 * :func:`sockets.createListenSocketIP`
+* :func:`sockets.connectByIPAddress`
+* :func:`sockets.acceptConnection`
 
 
 Function Reference
@@ -15,7 +17,76 @@ Function Reference
 
     :param string localAdress: The adress to bind to in string format. E.g. to bind to localhost on port 55556, use **"[::]:55556"**.
     :param table options: Table with key value pairs to override default SteamNetworkingSockets options. **NOT IMPLEMENTED YET**
-    :returns: a string representation of the adress/port connected to
+    :returns: nothing
     :SteamWorks: `CreateListenSocketIP <https://partner.steamgames.com/doc/api/ISteamNetworkingSockets#CreateListenSocketIP>`_
 
     Bind to an adress and accept connections as a "server". If you want to change any networking settings, you need pass these options on creation. Implement the callback **TODO** to be notified about connection events.
+
+**Example**::
+
+    Steam.sockets.createListenSocketIP("[::]:55556")
+
+.. function:: sockets.connectByIPAddress()
+
+    :param string localAdress: The adress to connect to in string format. E.g. to bind to localhost on port 55556, use **"127.0.0.1:55556"**.
+    :param table options: Table with key value pairs to override default SteamNetworkingSockets options. **NOT IMPLEMENTED YET**
+    :returns: nothing
+    :SteamWorks: `ConnectByIPAddress <https://partner.steamgames.com/doc/api/ISteamNetworkingSockets#ConnectByIPAddress>`_
+
+    Connect to a given adress as a "client". If you want to change any networking settings, you need pass these options on creation. Implement the callback **TODO** to be notified about connection events.
+
+**Example**::
+
+    Steam.sockets.connectByIPAddress("127.0.0.1:55556")
+    
+.. function:: sockets.acceptConnection()
+
+    :param int connection: The id of the connection to accept
+    :returns: string with the possible values ``OK | InvalidParam | InvalidState``
+    :SteamWorks: `AcceptConnection <https://partner.steamgames.com/doc/api/ISteamNetworkingSockets#AcceptConnection>`_
+
+    Accept a connection that was received via the callback **TODO**. This will move the connection from the "connecting" state to the "connected" state.
+
+**Example**::
+
+    local result = Steam.sockets.acceptConnection(connection)
+
+
+Callbacks Reference
+-------------------
+
+.. warning::
+
+    Remember callbacks are functions that you should override in order to receive the events, and not call directly.
+
+    Also, you **must** constantly call ``Steam.runCallbacks()`` (preferably in your game loop) in order for your callbacks to be called.
+
+.. function:: sockets.onConnectionChanged(data)
+
+    :param table data: Basic information about the changing connection
+
+		* **data.connection** (`string`)  a unique id representing this connection
+		* **data.state** (`string`)  the state this connection is now in
+		* **data.state_old** (`int`)  the previous state this connection was in
+    :returns: nothing
+    :SteamWorks: `SteamNetConnectionStatusChangedCallback_t <https://partner.steamgames.com/doc/api/ISteamNetworkingSockets#SteamNetConnectionStatusChangedCallback_t>`_
+
+    Posted whenever the state of a connection changes. For example
+        * a client attempts a new connection
+        * a server receives a new connection
+        * a connection is established successfully (client or server)
+        * a connection is closed (client or server)
+
+    Possible values for **data.state** are:
+        * None
+        * Connecting
+        * FindingRoute
+        * Connected
+        * ClosedByPeer
+        * ProblemDetectedLocally
+
+**Example**::
+
+    function Steam.sockets.onConnectionChanged(data)
+        print ('A connection has changed', data.connection, data.state, data.state_old)
+    end
--- src/sockets.cpp
+++ src/sockets.cpp
@@ -6,7 +6,14 @@
 
 namespace {
 
-HSteamListenSocket listenSocket;
+//ESteamNetworkingConnectionState
+const char *steam_networking_connection_state[] = {
+    "None", "Connecting", "FindingRoute", "Connected", "ClosedByPeer", "ProblemDetectedLocally", nullptr,
+};
+
+const char *steam_result_code[] = {
+ "None", "OK", "Fail", "NoConnection", "NoConnectionRetry", "InvalidPassword", "LoggedInElsewhere", "InvalidProtocolVer", "InvalidParam", "FileNotFound", "Busy", "InvalidState", "InvalidName", "InvalidEmail", "DuplicateName", "AccessDenied", "Timeout", "Banned", "AccountNotFound", "InvalidSteamID", "ServiceUnavailable", "NotLoggedOn", "Pending", "EncryptionFailure", "InsufficientPrivilege", "LimitExceeded", "Revoked", "Expired", "AlreadyRedeemed", "DuplicateRequest", "AlreadyOwned", "IPNotFound", "PersistFailed", "LockingFailed", "LogonSessionReplaced", "ConnectFailed", "HandshakeFailed", "IOFailure", "RemoteDisconnect", "ShoppingCartNotFound", "Blocked", "Ignored", "NoMatch", "AccountDisabled", "ServiceReadOnly", "AccountNotFeatured", "AdministratorOK", "ContentVersion", "TryAnotherCM", "PasswordRequiredToKickSession", "AlreadyLoggedInElsewhere", "Suspended", "Cancelled", "DataCorruption", "DiskFull", "RemoteCallFailed", "PasswordUnset", "ExternalAccountUnlinked", "PSNTicketInvalid", "ExternalAccountAlreadyLinked", "RemoteFileConflict", "IllegalPassword", "SameAsPreviousValue", "AccountLogonDenied", "CannotUseOldPassword", "InvalidLoginAuthCode", "AccountLogonDeniedNoMail", "HardwareNotCapableOfIPT", "IPTInitError", "ParentalControlRestricted", "FacebookQueryError", "ExpiredLoginAuthCode", "IPLoginRestrictionFailed", "AccountLockedDown", "AccountLogonDeniedVerifiedEmailRequired", "NoMatchingURL", "BadResponse", "RequirePasswordReEntry", "ValueOutOfRange", "UnexpectedError", "Disabled", "InvalidCEGSubmission", "RestrictedDevice", "RegionLocked", "RateLimitExceeded", "AccountLoginDeniedNeedTwoFactor", "ItemDeleted", "AccountLoginDeniedThrottle", "TwoFactorCodeMismatch", "TwoFactorActivationCodeMismatch", "AccountAssociatedToMultiplePartners", "NotModified", "NoMobileDevice", "TimeNotSynced", "SmsCodeFailed", "AccountLimitExceeded", "AccountActivityLimitExceeded", "PhoneActivityLimitExceeded", "RefundToWallet", "EmailSendFailure", "NotSettled", "NeedCaptcha", "GSLTDenied", "GSOwnerDenied", "InvalidItemType", "IPBanned", "GSLTExpired", "InsufficientFunds", "TooManyPending", "NoSiteLicensesFound", "WGNetworkSendExceeded", "AccountNotFriends", "LimitedUserAccount", "CantRemoveItem", "AccountDeleted", "ExistingUserCancelledLicense", "CommunityCooldown", "NoLauncherSpecified", "MustAgreeToSSA", "LauncherMigrated", "SteamRealmMismatch", "InvalidSignature", "ParseFailure", "NoVerifiedPhone", "InsufficientBattery", "ChargerRequired", "CachedCredentialInvalid", "PhoneNumberIsVOIP", nullptr,
+};
 
 class CallbackListener;
 CallbackListener *connection_listener = nullptr;
@@ -26,18 +33,29 @@ void CallbackListener::OnConnectionChanged(SteamNetConnectionStatusChangedCallba
         return;
     }
     lua_rawgeti(L, LUA_REGISTRYINDEX, sockets_ref);
-    lua_getfield(L, -1, "onGameOverlayActivated");
+    lua_getfield(L, -1, "onConnectionChanged");
     if (lua_isnil(L, -1)) {
         lua_pop(L, 2);
     } else {
         lua_createtable(L, 0, 1);
-        CSteamID steamID = data->m_info.m_identityRemote.GetSteamID()
-        if (steamID.is_Valid()) {
-            lua_pushinteger(L, steamID.ConvertToUint64());
-        } else {
-            lua_pushinteger(L, -1);
-        }
-        lua_setfield(L, -2, "steam_id");
+
+        // first save current and old state
+        lua_pushstring(L, steam_networking_connection_state[data->m_info.m_eState]);
+        lua_setfield(L, -2, "state");
+        lua_pushstring(L, steam_networking_connection_state[data->m_eOldState]);
+        lua_setfield(L, -2, "state_old");
+        lua_pushinteger(L, data->m_hConn);
+        lua_setfield(L, -2, "connection");
+
+        //int64 steamIDNumber = -1;
+        //CSteamID steamID = data->m_info.m_identityRemote.GetSteamID();
+        //if (steamID.IsValid()) {
+        //    steamIDNumber = steamID.ConvertToUint64();
+        //}
+        //char *steamIDString = nullptr;
+        //sprintf(steamIDString, "%lld", steamIDNumber);
+        //lua_pushstring(L, steamIDString);
+        //lua_setfield(L, -2, "steam_id");
         lua_call(L, 1, 0);
         lua_pop(L, 1);
     }
@@ -47,41 +65,52 @@ void CallbackListener::OnConnectionChanged(SteamNetConnectionStatusChangedCallba
 
 // HSteamListenSocket CreateListenSocketIP( const SteamNetworkingIPAddr &localAddress, int nOptions, const SteamNetworkingConfigValue_t *pOptions )
 EXTERN int luasteam_createListenSocketIP(lua_State *L) {
-    fprintf(stderr, "Adding sockets %s\n", luaL_checkstring(L, 1));
     SteamNetworkingIPAddr localAdress;
     localAdress.ParseString(luaL_checkstring(L, 1));
+    // TODO read options from state
+    HSteamListenSocket connectingSocket = SteamNetworkingSockets()->CreateListenSocketIP(localAdress, 0, nullptr);
+    return 0;
+}
 
-    // TODO read the options table { max = 5, min = 3}
-    // int options = luaL_checkint(L, 2);
-    listenSocket = SteamNetworkingSockets()->CreateListenSocketIP(localAdress, 0, nullptr);
+// HSteamNetConnection ConnectByIPAddress( const SteamNetworkingIPAddr &address, int nOptions, const SteamNetworkingConfigValue_t *pOptions )
+EXTERN int luasteam_connectByIPAddress(lua_State *L) {
+    SteamNetworkingIPAddr address;
+    address.ParseString(luaL_checkstring(L, 1));
+    // TODO read options from state
+    HSteamListenSocket connectingSocket = SteamNetworkingSockets()->ConnectByIPAddress(address, 0, nullptr);
+    return 0;
+}
 
-    size_t cbBuf = 64;
-    char *connectionString = new char[cbBuf];
-    localAdress.ToString(connectionString, cbBuf, true);
-    lua_pushstring(L, connectionString);
-
-    // lua_createtable(L, 0, 1);
-    // lua_pushstring(L, connectionString);
-    // lua_setfield(L, -2, "connection");
+// EResult AcceptConnection( HSteamNetConnection hConn )
+EXTERN int luasteam_acceptConnection(lua_State *L) {
+    HSteamNetConnection hConn = luaL_checkinteger(L, 1);
+    EResult result = SteamNetworkingSockets()->AcceptConnection(hConn);
+    lua_pushstring(L, steam_result_code[result]);
     return 1;
 }
 
 namespace luasteam {
 
 void add_sockets(lua_State *L) {
-    fprintf(stderr, "Adding sockets\n");
-    lua_createtable(L, 0, 1);
+    lua_createtable(L, 0, 3);
     add_func(L, "createListenSocketIP", luasteam_createListenSocketIP);
+    add_func(L, "connectByIPAddress", luasteam_connectByIPAddress);
+    add_func(L, "acceptConnection", luasteam_acceptConnection);
     lua_pushvalue(L, -1);
     sockets_ref = luaL_ref(L, LUA_REGISTRYINDEX);
     lua_setfield(L, -2, "sockets");
 }
 
-void init_sockets(lua_State *L) { fprintf(stderr, "Initiating sockets\n"); }
+void init_sockets(lua_State *L) {
+    connection_listener = new CallbackListener();
+}
 
-void shutdown_sockets(lua_State *L) { 
+void shutdown_sockets(lua_State *L) {
     luaL_unref(L, LUA_REGISTRYINDEX, sockets_ref);
     sockets_ref = LUA_NOREF;
-    fprintf(stderr, "Shutting down sockets\n"); }
+
+    delete connection_listener;
+    connection_listener = nullptr;
+}
 
 } // namespace luasteam
