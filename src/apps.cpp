b899918505dfc201163f555c1948e999ed330105
Kevin Harrison <keharriso@gmail.com>
2023-10-14 13:13:34 -0400

    Add getLaunchCommandLine

--- src/apps.cpp
+++ src/apps.cpp
@@ -10,11 +10,21 @@ EXTERN int luasteam_getCurrentGameLanguage(lua_State *L) {
   return 1;
 }
 
+// int GetLaunchCommandLine( char *pszCommandLine, int cubCommandLine );
+EXTERN int luasteam_getLaunchCommandLine(lua_State *L) {
+  char buffer[256];
+  int bytes = SteamApps()->GetLaunchCommandLine(buffer, sizeof(buffer) - 1);
+  buffer[bytes] = '\0';
+  lua_pushstring(L, buffer);
+  return 1;
+}
+
 namespace luasteam {
 
 void add_apps(lua_State *L) {
     lua_createtable(L, 0, 1);
     add_func(L, "getCurrentGameLanguage", luasteam_getCurrentGameLanguage);
+    add_func(L, "getLaunchCommandLine", luasteam_getLaunchCommandLine);
     lua_setfield(L, -2, "apps");
 }
 
