de70a64d47d1b9ffadbd3e2ff0c4565a52c5c475
Cartread <69614530+Cartread@users.noreply.github.com>
2024-09-04 00:46:21 -0400

    Update utils.cpp
    no errors; untested

--- src/utils.cpp
+++ src/utils.cpp
@@ -4,18 +4,16 @@
 // ======= SteamUtils =======
 // ==========================
 
-
 using luasteam::CallResultListener;
 
 namespace {
 
 class CallbackListener;
 CallbackListener *callback_listener = nullptr;
-int userStats_ref = LUA_NOREF;
+int utils_ref = LUA_NOREF;
 
-//const char *sort_methods[] = {"Ascending", "Descending", nullptr};
-//const char *display_types[] = {"Numeric", "TimeSeconds", "TimeMilliSeconds", nullptr};
-
+const char *InputModes[] = {"Normal", "Password", nullptr};
+const char *InputLineModes[] = {"SingleLine", "MultipleLines", nullptr};
 
 class CallbackListener {
   private:
@@ -23,14 +21,14 @@ class CallbackListener {
 };
 
 void CallbackListener::OnGamepadTextInputDismissed(GamepadTextInputDismissed_t *data) {
-    if (data == nullptr) {
+    if (data == nullptr || !data->m_bSubmitted) { // The user cancelled
         return;
     }
     lua_State *L = luasteam::global_lua_state;
     if (!lua_checkstack(L, 4)) {
         return;
     }
-    lua_rawgeti(L, LUA_REGISTRYINDEX, userStats_ref);
+    lua_rawgeti(L, LUA_REGISTRYINDEX, utils_ref);
     lua_getfield(L, -1, "onGamepadTextInputDismissed");
     if (lua_isnil(L, -1)) {
         lua_pop(L, 2);
@@ -51,21 +49,14 @@ void CallbackListener::OnGamepadTextInputDismissed(GamepadTextInputDismissed_t *
 
 } // namespace
 
-
-
-
-
-
 // uint32 GetAppID();
 EXTERN int luasteam_getAppID(lua_State *L) {
     lua_pushnumber(L, SteamUtils()->GetAppID());
     return 1;
 }
 
-
-
 // bool GetEnteredGamepadTextInput( char *pchText, uint32 cchText );
-EXTERN bool luasteam_getEnteredGamepadTextInput(lua_State *L) {
+EXTERN int luasteam_getEnteredGamepadTextInput(lua_State *L) {
     char pchText[1024];
     SteamUtils()->GetEnteredGamepadTextInput(pchText, 1024);
     lua_pushstring(L, pchText);
@@ -78,17 +69,17 @@ EXTERN int luasteam_getEnteredGamepadTextLength(lua_State *L) {
     return 1;
 }
 
-//SIMPLE AND O-O-O (0,0,cc,int,cc)
 //bool ShowGamepadTextInput( EGamepadTextInputMode eInputMode, EGamepadTextInputLineMode eLineInputMode, const char *pchDescription, uint32 unCharMax, const char *pchExistingText );
-    //int sort_method = luaL_checkoption(L, 2, nullptr, sort_methods) + 1;
-    //int display_type = luaL_checkoption(L, 3, nullptr, display_types) + 1;
 EXTERN int luasteam_showGamepadTextInput(lua_State *L) {
-    char pchDescription[1024];
-    const char *url = luaL_checkstring(L, 1);//O-O-O!
-    SteamUtils()->ShowGamepadTextInput(url,0,0,pchDescription, 1024);
+
+    int InputMode = luaL_checkoption(L, 1, nullptr, InputModes) + 1;
+    int InputLineMode = luaL_checkoption(L, 2, nullptr, InputLineModes) + 1;
+    //char pchDescription[1024];
+    const char *pchDescription = luaL_checkstring(L, 3);
+    const char *pchExistingText = luaL_checkstring(L, 5);
+    SteamUtils()->ShowGamepadTextInput(static_cast<EGamepadTextInputMode>(InputMode), static_cast<EGamepadTextInputLineMode>(InputLineMode), pchDescription, 1024, pchExistingText);
     return 1;
 }
-
 
 namespace luasteam {
 
@@ -112,6 +103,5 @@ void shutdown_utils(lua_State *L) {
     delete callback_listener;
     callback_listener = nullptr;
 }
-
 
 } // namespace luasteam
