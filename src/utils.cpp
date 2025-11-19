e5031d9c7a957fbee0ea9e174daf5c4893dd3ef6
Alex Larioza <contact@gamesrightmeow.com>
2025-11-19 08:14:02 -0800

    added isSteamRunningOnSteamDeck

--- src/utils.cpp
+++ src/utils.cpp
@@ -10,11 +10,18 @@ EXTERN int luasteam_getAppID(lua_State *L) {
     return 1;
 }
 
+// bool IsSteamRunningOnSteamDeck();
+EXTERN int luasteam_isSteamRunningOnSteamDeck(lua_State *L) {
+    lua_pushnumber(L, SteamUtils()->IsSteamRunningOnSteamDeck());
+    return 1;
+}
+
 namespace luasteam {
 
 void add_utils(lua_State *L) {
     lua_createtable(L, 0, 1);
     add_func(L, "getAppID", luasteam_getAppID);
+    add_func(L, "isSteamRunningOnSteamDeck", luasteam_isSteamRunningOnSteamDeck);
     lua_setfield(L, -2, "utils");
 }
 
